44e3c2985ebbf8344b3a8cf0468734ffd299bf01
Bo Liu <rosshop86@gmail.com>
2016-02-14 19:10:11 -0600

    update AccelGyroMag tests

--- C++/Examples/AccelGyroMag/AccelGyroMag.cpp
+++ C++/Examples/AccelGyroMag/AccelGyroMag.cpp
@@ -1,49 +1,65 @@
-/*
-Provided to you by Emlid Ltd (c) 2014.
-twitter.com/emlidtech || www.emlid.com || info@emlid.com
-
-Example: Read accelerometer, gyroscope and magnetometer values from
-MPU9250 inertial measurement unit over SPI on Raspberry Pi + Navio.
-
-Navio's onboard MPU9250 is connected to the SPI bus on Raspberry Pi
-and can be read through /dev/spidev0.1
-
-To run this example navigate to the directory containing it and run following commands:
-make
-./AccelGyroMag
+/**
+ * @file AccelGyroMap.cpp
+ *
+ * test MPU9250
+ *
+ * @auhor Bo Liu (bo-rc@acm.org)
+ *
+ * Re-engineered implementations from emlid
 */
 
 #include "Navio/MPU9250.h"
 #include <iostream>
+#inlcude <cstdlib>
 
 //=============================================================================
 
-int main()
+int main(int argc, char * argv[])
 {
-	//-------------------------------------------------------------------------
+    int loop_num = 500;
+    if (arg > 1)
+    {
+        loop_num = atoi(argv[1]);
+    }
+
+    std::cout << "type Q to quit" << std::endl;
 
 	MPU9250 imu;
 	imu.initialize();
 
 	float ax, ay, az, gx, gy, gz, mx, my, mz;
 
-    uint8_t addr = imu.whoami();
-
-    std::cout << "MPU9250 address: " << std::hex << addr << std::endl;
+    std::cout << "MPU9250 address: " << std::hex << int(imu.whoami()) << std::endl;
 
     if(imu.testConnection())
     {
         std::cout << "MPU9250 is connected" << std::endl;
     }
 
-    //-------------------------------------------------------------------------
+    while (cin != q) {
+        std::cout << "before Acc calibration:" << std::endl;
 
-    while(1) {
-        imu.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
-        printf("Acc: %+7.3f %+7.3f %+7.3f  ", ax, ay, az);
-        printf("Gyr: %+8.3f %+8.3f %+8.3f  ", gx, gy, gz);
-        printf("Mag: %+7.3f %+7.3f %+7.3f\n", mx, my, mz);
-	
-        usleep(500000);
+        for(int i = 0; i != loop_num; ++i) {
+            imu.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
+            printf("Acc: %+7.3f %+7.3f %+7.3f  ", ax, ay, az);
+            printf("Gyr: %+8.3f %+8.3f %+8.3f  ", gx, gy, gz);
+            printf("Mag: %+7.3f %+7.3f %+7.3f\n", mx, my, mz);
+
+            usleep(500000);
+        }
+
+        imu.calib_acc();
+        std::cout << "after Acc calibration: " << std::endl;
+
+        for(int i = 0; i != loop_num; ++i) {
+            imu.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
+            printf("Acc: %+7.3f %+7.3f %+7.3f  ", ax, ay, az);
+            printf("Gyr: %+8.3f %+8.3f %+8.3f  ", gx, gy, gz);
+            printf("Mag: %+7.3f %+7.3f %+7.3f\n", mx, my, mz);
+
+            usleep(500000);
+        }
     }
+
+    return 0;
 }
